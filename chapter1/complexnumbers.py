c1334920e79234da9cdcd116897b74db69473547
petahimabindu <petahimabindu1@gmail.com>
2026-08-17 21:23:23 +0530

    Create complexnumbers.py

---
+++ chapter1/complexnumbers.py
@@ -0,0 +1,11 @@
+#Read first complex number
+a=complex(input("enter first complex number:"))
+#Read second complex number
+b=complex(input("enter second complex number:"))
+#Addition
+addition=a+b
+#multiplication
+multiplication=a*b
+
+print("Addition=",addition)
+print("Multiplication=",multiplication)
