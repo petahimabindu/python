cba6c2f14badea7c3a7d276d4b44e08f8f58ab7a
petahimabindu <petahimabindu1@gmail.com>
2026-08-21 21:01:00 +0530

    Create search for numbers.py

---
+++ chapter1/search for numbers.py
@@ -0,0 +1,9 @@
+numbers=[10,20,30,40,50,34,45]
+key=int(input("enter the key value:"))
+
+for n in numbers:
+  if n==key:
+    print("number is found")
+  else:
+    print("number is not found")
+    
