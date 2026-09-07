3b169a02c733b63690c7f522831d02e8ae0fe24d
petahimabindu <petahimabindu1@gmail.com>
2026-09-07 21:27:28 +0530

    Create Operations on a list.py

---
+++ chapter 2/Operations on a list.py
@@ -0,0 +1,11 @@
+# Program to perform addition operation on a list
+
+list1 = [10, 20, 30, 40]
+
+print("Original list:", list1)
+
+element = int(input("Enter an element to add: "))
+
+list1.append(element)
+
+print("List after addition:", list1)
