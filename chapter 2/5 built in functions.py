f864e8a1bab037dab7b1a21ec70d5f4c2d3e5cf6
petahimabindu <petahimabindu1@gmail.com>
2026-09-11 22:31:55 +0530

    Create 5 built in functions.py

---
+++ chapter 2/5 built in functions.py
@@ -0,0 +1,11 @@
+# Program to perform 5 built-in functions on a list
+
+list1 = [10, 20, 5, 40, 15]
+
+print("List:", list1)
+
+print("Length of list:", len(list1))
+print("Maximum element:", max(list1))
+print("Minimum element:", min(list1))
+print("Sum of elements:", sum(list1))
+print("Sorted list:", sorted(list1))
