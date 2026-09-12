ef64b08abcaea059c83a671b631e5445a9901dc0
petahimabindu <petahimabindu1@gmail.com>
2026-09-12 06:40:36 +0530

    Create Concatenation of tuples.py

---
+++ chapter 2/Concatenation of tuples.py
@@ -0,0 +1,11 @@
+# Create two tuples
+tuple1 = (10, 20)
+tuple2 = (30, 40)
+
+# Concatenate the tuples
+tuple3 = tuple1 + tuple2
+
+# Display the tuples
+print("Tuple 1:", tuple1)
+print("Tuple 2:", tuple2)
+print("Concatenated Tuple:", tuple3)
